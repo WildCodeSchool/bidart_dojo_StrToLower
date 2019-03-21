f91fa04a20a0d2b3ce3a0c90dfdf93d13e608ccb
Stéphane Vivier <contact.xsprod@gmail.com>
2019-03-21 11:54:09 +0100

    Modifified stdin method syntax and name of test file

--- bonusProcessStrToLower.js
+++ bonusProcessStrToLower.js
@@ -1,7 +1,7 @@
 process.stdin.resume()
 process.stdin.setEncoding('utf8')
 
-console.log('Enter a string with Uppercase :')
+console.log('Enter a string with Uppercase characters:')
 process.stdin.on('data', (str) => {
     console.log(str.toLowerCase());
     process.exit()
